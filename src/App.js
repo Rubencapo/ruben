18b948c70d3e450208181bf87794809cc1fa1560
Rubén <rubencaporte@gmail.com>
2022-07-30 02:15:54 +0200

    thirt commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <h1>RUBEN PORTE</h1>
-          <p>My webstite under construccion</p>
-        </div>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
